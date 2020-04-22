c30edd828c40b3700bb34b551c4147499e7c6a44
Ondrej Hudecek <3dhudy@gmail.com>
2020-04-22 20:57:27 +0200

    Update

--- Lesson02/Destinatio02.py
+++ Lesson02/Destinatio02.py
@@ -4,7 +4,7 @@
 # ~~~~~~~~~~~~~~~~~~~ZAPIS Z LEKCE #01~~~~~~~~~~~~~~~~~~~
 SEZNAM_MEST = ("Praha", "Viden", "Olomouc", "Svitavy", "Zlin", "Ostrava")
 SEZNAM_CEN = (150, 200, 120, 120, 100, 180)
-ODDELOVAC = "=" * 35
+ODDELOVAC = "=" * 40
 
 print(ODDELOVAC)
 print("Vitejte u nasi aplikace Destinatio!")
