c475dc3bad5942580b1666d3e5d6a3378bfd8f27
Ondrej Hudecek <3dhudy@gmail.com>
2020-05-01 18:55:46 +0200

    TextAnalyzator

--- Project01/textAnalyzator.py
+++ Project01/textAnalyzator.py
@@ -1,24 +1,30 @@
 from pprint import pprint as pp
 
-oddelovac = '=' * 40
-uvivatele = {
+ODDELOVAC = '=' * 40
+UZIVATELE = {
     'bob' : '123', 
     'ann' : 'pass123', 
     'mike' : 'password123', 
     'liz' : 'pass123'
 }
 
+# TEXTS = 
+
 # 1. Na začátku přivítá uživatele.
-print(oddelovac)
+print(ODDELOVAC)
 print('Vytejte v aplikaci, prihlaste se prosim: ')
-print(oddelovac)
+print(ODDELOVAC)
 
 # 2. Vyžádá si od uživatele přihlašovací jméno a heslo. 
-jmeno = str(input('Zadejte jmeno: '))
-heslo = str(input('Zadejte heslo: '))
+jmeno = str(input('Zadejte jmeno:'))
+heslo = str(input('Zadejte heslo:'))
 
 # 3. Zjistí, jestli zadané údaje odpovídají někomu z registrovaných uživatelů.
+if UZIVATELE.get(jmeno) != heslo:
+    print('Jmeno nebo heslo neni spravne!')
 
+elif UZIVATELE.get(jmeno) == heslo:
+    print('Muzete pokracovat.')
 
 # 4. Program nechá uživatele vybrat mezi třemi texty, uloženými v proměnné TEXTS. 
 
