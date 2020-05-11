1f1beb04b94b593d6da4fefa354edd7112c1c9d3
Ondrej Hudecek <3dhudy@gmail.com>
2020-05-11 18:00:28 +0200

    Lesson07

---
+++ Lesson07/calculator.py
@@ -0,0 +1,34 @@
+"""Lekce #7 - Uvod do programovani, kalkulacka"""
+
+# I. KROK
+# Zadame pomocne promenne
+NABIDKA = "DOSTUPNE OPERACE: "
+OPERACE = "+", "-", "*", "/", "prum", "^"
+ODDELOVAC = "-" * 25
+
+# II. KROK
+# Vytvorit hlavni ridici funkci
+
+# III. KROK
+# Uvadeci funkce
+
+# IV. KROK
+# Rozcestnik funkci
+
+# V. KROK
+# Potrebujeme dva vstupy
+
+# VI. KROK
+# Resime zakladni aritmeticke operatory
+
+# VII. KROK
+# Potrebujeme radu cisel pro prumer
+
+# VIII. KROK
+# Pocitam prumer z pomocne promenne
+
+# IX. KROK
+# Umocnim cislo, exponentem
+
+# ad. II. KROK
+# Volam svoji hlavni funkci
